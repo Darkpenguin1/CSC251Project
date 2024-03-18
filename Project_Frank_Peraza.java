1d401149b20d172b643a12a8d89784f44d130bab
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-03-18 17:36:06 -0400

    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Frank_Peraza.java
+++ Project_Frank_Peraza.java
@@ -10,6 +10,9 @@ public class Project_Frank_Peraza{
       Scanner keyboard = new Scanner(file);         // init the scanner class to read in file info
       ArrayList<Policy> policyList = new ArrayList<Policy>();
       
+      int nonSmokerCount = 0; // init smoker/non smoker counter variables
+      int smokerCount = 0;
+      
       
       while (keyboard.hasNextLine()){     // A while loop that runs until there is no more file info
          int policyNum = keyboard.nextInt();          // Read in the file info into file scanner and assign the info to the variables accordingly
@@ -39,11 +42,21 @@ public class Project_Frank_Peraza{
          System.out.println("\nPolicyholders last name: " + policyList.get(i).getPolicyHolderLastName());
          System.out.println("\nPolicyholders age: " + policyList.get(i).getPolicyHolderAge());
          System.out.println("\nPolicyholders smoking status: " + policyList.get(i).getPolicyHolderSmokingStatus());
+         
+         if (policyList.get(i).getPolicyHolderSmokingStatus().equalsIgnoreCase("smoker")){   // if statements that check if the policy holder smokes or not and counts it towards the running total
+            smokerCount++;
+         }
+         else{
+            nonSmokerCount++;
+         }
+         
          System.out.println("\nPolicyholders height: " + policyList.get(i).getPolicyHolderHeight());
          System.out.println("\nPolicyholders weight: " + policyList.get(i).getPolicyHolderWeight());
          System.out.printf("\nPolicyholders BMI: %.2f\n", + policyList.get(i).getBMI());
          System.out.printf("\nPolicy Price: $%.2f\n", + policyList.get(i).getInsurancePolicyPrice());
       }
+      System.out.println("\nThe number of policies with a smoker is: " + smokerCount);
+      System.out.println("\nThe number of policies with a non-smoker is: " + nonSmokerCount);
       keyboard.close();    // make sure to close the file! 
    }
 }
